d0f8fe32cb3c3f9b804d4147e350e4de0ca622f1
Jacob <jacob.j.lee43@gmail.com>
2020-09-14 09:38:13 -0400

    Testing this Commit
    Does this work?

--- OVRInputTesting.cs
+++ OVRInputTesting.cs
@@ -7,7 +7,7 @@ public class OVRInputTesting : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        //Let's see if gitkraken works
     }
 
     // Update is called once per frame

11259530f59eef466aeb33fe1b036b9ab5474800
Jacob <jacob.j.lee43@gmail.com>
2020-10-08 10:32:56 -0400

    Added updated code Reupload

--- Assets/Oculus/VR/Scripts/Util/OVRGrabbableJacob.cs
+++ Assets/Oculus/VR/Scripts/Util/OVRGrabbableJacob.cs
@@ -7,6 +7,13 @@ public class OVRGrabbableJacob : OVRGrabbable
 {
     public override void GrabEnd(Vector3 linearVelocity, Vector3 angularVelocity, Vector3 cross)
     {
-        
+        Rigidbody rb = gameObject.GetComponent<Rigidbody>();
+        rb.isKinematic = m_grabbedKinematic;
+        Vector3 fullVelocity = linearVelocity + cross;
+
+        rb.velocity = fullVelocity;
+        rb.angularVelocity = angularVelocity;
+        m_grabbedBy = null;
+        m_grabbedCollider = null;
     }
 }

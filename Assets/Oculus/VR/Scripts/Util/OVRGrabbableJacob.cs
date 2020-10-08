3d3bdc5c4aa63971dad0afdbfac5b49d5249603f
Jacob <jacob.j.lee43@gmail.com>
2020-10-08 09:06:11 -0400

    Adding OVRGrabbableJacob Added

---
+++ Assets/Oculus/VR/Scripts/Util/OVRGrabbableJacob.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using CircularBuffer;
+
+public class OVRGrabbableJacob : OVRGrabbable
+{
+    public override void GrabEnd(Vector3 linearVelocity, Vector3 angularVelocity, Vector3 cross)
+    {
+        
+    }
+}
